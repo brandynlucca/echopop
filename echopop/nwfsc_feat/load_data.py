89de363a6407a3316c26501ac850ecc49f08f797
Wu-Jung Lee <leewujung@gmail.com>
2025-04-28 20:37:10 -0700

    tweak load_data functions

--- echopop/nwfsc_feat/load_data.py
+++ echopop/nwfsc_feat/load_data.py
@@ -6,13 +6,15 @@ import pandas as pd
 # Break up load_data() and read_validated_data() into 3 separate functions to load biological, stratification, and kriging data
 # Don't worry about validating input files for now
 
-# TODO: vario_krig_para is currently not used, right? If so, remove any related code
 
-
+# TODO: read from the master spreadsheet
 # TODO: combine in content of preprocess_biodata()
-def load_biological_data(root_path: Union[str, Path], file_path_dict: Dict) -> Dict[pd.DataFrame]:
+def load_biological_data(
+    root_path: Union[str, Path],
+    file_path_dict: Dict
+) -> Dict[pd.DataFrame]:
     """
-    Load biological data from CSV
+    Load biological data from master biological data spreadsheet.
 
     Parameters
     ----------
@@ -23,14 +25,21 @@ def load_biological_data(root_path: Union[str, Path], file_path_dict: Dict) -> D
 
     Returns
     -------
-    A dictionary of dataframes containing biological data
+    df_length : pd.DataFrame
     """
-    df_bio_dict: Dict
+    df_bio_dict: Dict = {
+        "length": pd.DataFrame,
+        "specimen": pd.DataFrame,
+        "catch": pd.DataFrame,
+    }
     return df_bio_dict
 
 
 # TODO: combine in the current preprocess_spatial()
-def load_stratification(root_path: Union[str, Path], file_path_dict: Dict) -> Dict[pd.DataFrame]:
+def load_stratification(
+    root_path: Union[str, Path],
+    file_path_dict: Dict,
+) -> Dict[pd.DataFrame]:
     """
     Load stratification schemes from CSV
 
@@ -49,13 +58,19 @@ def load_stratification(root_path: Union[str, Path], file_path_dict: Dict) -> Di
     # `bio_strata` is the current `strata`
 
     # In addition to the original operations in preprocess_spatial()
-    # also do stratum renaming for inpfc originall in transect.py::save_transect_coordinates()
+    # also do stratum renaming for inpfc originally in transect.py::save_transect_coordinates()
+    # the two output dataframes should have the same column names
+    #
+    # BELOW CODE SNIPPET FROM save_transect_coordinates()
     # if stratum_def == "inpfc":
     #     stratum_rename = "stratum_num"
     # else:
     #     stratum_rename = stratum_col
 
-    df_strata_dict: Dict
+    df_strata_dict: Dict = {
+        "ks": pd.DataFrame,
+        "inpfc": pd.DataFrame,
+    }
     return df_strata_dict
 
 
@@ -78,12 +93,29 @@ def join_acoustic_all(
     df_nasc: pd.DataFrame,
     df_bio_dict: Dict[pd.DataFrame],
     df_strata_dict: Dict[pd.DataFrame],
-    species_code
+    species_code: str,
 ) -> pd.DataFrame:
     return df_nasc
 
 
-# NOTE: combine content of preprocess_statistics()
+def consolidate_all_data(
+    df_nasc: pd.DataFrame,
+    df_bio_dict: Dict[pd.DataFrame],
+    df_strata_dict: Dict[pd.DataFrame],
+    species_code: str
+) -> pd.DataFrame:
+    """
+    Consolidate all input data.
+    """
+    df_bio_dict = join_biological_stratification(df_bio_dict, df_strata_dict)
+    df_nasc = join_acoustic_stratification(df_nasc, df_strata_dict)
+    df_nasc = join_acoustic_all(df_nasc, df_bio_dict, df_strata_dict, species_code)
+    
+    return df_nasc
+
+
+
+# TODO: combine content of preprocess_statistics()
 def load_kriging_templates(
     root_path: Union[str, Path], file_path_dict: Dict
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
--- echopop/workflow/feat_hake.py
+++ echopop/workflow/feat_hake.py
@@ -36,8 +36,7 @@ df_nasc_no_age1 = ingest_echoview_nasc.consolidate_echoview_nasc(
 
 # Use regular pd.read_csv to read df_nasc_*, effectively break up the current load_data()
 # -- there is no need to have a one-size-fits-all load_data function
-# -- you can validate the biological, stratification, and NASC data
-# -- but just read them in is fine: these are all files under our control
+# -- just read them in without validation is fine: these are all files under our control
 
 
 
@@ -48,26 +47,28 @@ df_nasc_no_age1 = ingest_echoview_nasc.consolidate_echoview_nasc(
 
 root_path = "WHERE_ALL_DATA_ARE"
 species_code = "SPECIES_CODE"
-df_nasc_no_age1: pd.DataFrame  # extracted nasc data from above
+df_nasc_no_age1: pd.DataFrame  # extracted nasc data from above, can also be df_nasc_all_ages
 
 bio_path_dict: dict  # the "biological" section of year_config.yml
+                     # this will be simplified now that we read from the master spreadsheet
 strata_path_dict: dict  # the "stratification" section of year_config.yml
-kriging_path_dict: dict  # the "kriging" section of year_config.yml
-                         # combined with the "kriging" section of init_config.yml
 
-df_bio_dict: Dict[pd.DataFrame] = load_data.load_biological_data(root_path, file_path_dict=bio_path_dict)
-
-df_strata_dict: Dict[pd.DataFrame] = load_data.load_stratification(root_path, file_path_dict=strata_path_dict)
+df_bio_dict: Dict[pd.DataFrame] = load_data.load_biological_data(root_path, bio_path_dict)
+df_strata_dict: Dict[pd.DataFrame] = load_data.load_stratification(root_path, strata_path_dict)
 
 # Consolidate all input data into df_acoustic_dict
-df_bio_dict = load_data.join_biological_stratification(df_bio_dict, df_strata_dict)
-df_nasc_no_age1 = load_data.join_acoustic_stratification(df_nasc_no_age1, df_strata_dict)
-df_nasc_no_age1 = load_data.join_acoustic_all(df_nasc_no_age1, df_bio_dict, df_strata_dict, species_code)
-
+df_nasc_no_age1 = load_data.consolidate_all_data(
+    df_nasc=df_nasc_no_age1,
+    df_bio_dict=df_bio_dict,
+    df_strata_dict=df_strata_dict
+)
 
 
 
 # ===========================================
 # Load kriging-related params and templates
+kriging_path_dict: dict  # the "kriging" section of year_config.yml
+                         # combined with the "kriging" section of init_config.yml
+
 df_mesh_template, df_isobath = load_data.load_kriging_templates(root_path, kriging_path_dict)
 kriging_param_dict, variogram_params_dict = load_data.load_kriging_variogram_params(root_path, kriging_path_dict)
