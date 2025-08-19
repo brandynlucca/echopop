3078cd16c9e58b6da9093e84c14a73068e42ecd1
Brandyn Lucca <brandyn.lucca@gmail.com>
2025-08-18 20:44:42 -0700

    Additional merge and pre-commit fixes

--- echopop/nwfsc_feat/utils.py
+++ echopop/nwfsc_feat/utils.py
@@ -722,15 +722,6 @@ class TSLRegressionParameters(InputModel, title="TS-length regression parameters
     slope: float = Field(allow_inf_nan=False)
     intercept: float = Field(allow_inf_nan=False)
 
-
-from typing import Any, Dict, List, Optional, Union
-
-import numpy as np
-import numpy.typing as npt
-import pandas as pd
-from scipy import interpolate as interp
-
-
 def roundn(n):
     """
     Rounding function to avoid bankers rounding
